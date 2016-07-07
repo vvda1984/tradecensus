f7f0ecdfcad1788a3c5bb8dc24ac17c26ad88563
vvda1984 <vvda1984@gmail.com>
2016-07-07 17:22:33 +0700

    add missing files

---
+++ TradeCensusMobile/TradeCensusMobile/www/js/createOutletController.js
@@ -0,0 +1,62 @@
+﻿function newOutletController($scope, $mdDialog) {
+    $scope.capture = function (i) {
+        captureImage(function (imageURI) {
+            var image = document.getElementById('outletImg' + i.toString());
+            image.src = imageURI;
+            //var image = document.getElementById('outletImg' + i);
+            //image.src = "data:image/jpeg;base64," + imageData;
+        }, function (err) {
+            showError(err);
+        });
+    }
+
+    $scope.saveUpdate = function () {
+        if (isEmpty($scope.outlet.Name)) {
+            showError('Outlet name is empty!');
+            return;
+        }
+        if (isEmpty($scope.outlet.AddLine)) {
+            showError('House number is empty!');
+            return;
+        }
+        if (isEmpty($scope.outlet.AddLine2)) {
+            showError('Street is empty!');
+            return;
+        }
+        if (isEmpty($scope.outlet.District)) {
+            showError('District is empty!');
+            return;
+        }
+        if (isEmpty($scope.outlet.Phone)) {
+            showError('Phone is empty!');
+            return;
+        }
+        if (isEmpty($scope.outlet.TotalVolume)) {
+            showError('Total is empty!');
+            return;
+        }
+        if (isEmpty($scope.outlet.VBLVolume)) {
+            showError('VBL Volume is empty!');
+            return;
+        }
+
+        $mdDialog.hide(true);
+    };
+
+    $scope.cancelUpdate = function () {
+        $mdDialog.cancel();
+    };
+
+    function captureImage(onSuccess, onError) {
+        try {
+            navigator.camera.getPicture(onSuccess, onError,
+                {
+                    quality: 50,
+                    correctOrientation: true,
+                    destinationType: Camera.DestinationType.FILE_URI // DATA_URL for base64 => not recommend due to memory issue
+                });
+        } catch (err) {
+            showError(err);
+        }
+    }
+}
---
+++ TradeCensusMobile/TradeCensusMobile/www/js/editOutletController.js
@@ -0,0 +1,34 @@
+﻿function editOutletController($scope, $mdDialog) {
+    $scope.capture = function (i) {
+        captureImage(function (imageURI) {
+            var image = document.getElementById('outletImg' + i.toString());
+            image.src = imageURI;
+            //var image = document.getElementById('outletImg' + i);
+            //image.src = "data:image/jpeg;base64," + imageData;
+        }, function (err) {
+            showError(err);
+        });
+    }
+    $scope.openedOptionVisible = !$scope.outlet.IsOpened;
+    $scope.trackedOptionVisible = !$scope.outlet.IsTracked;
+    $scope.saveUpdate = function () {
+        $mdDialog.hide(true);
+    };
+
+    $scope.cancelUpdate = function () {
+        $mdDialog.cancel();
+    };
+
+    function captureImage(onSuccess, onError) {
+        try {
+            navigator.camera.getPicture(onSuccess, onError,
+                {
+                    quality: 50,
+                    correctOrientation: true,
+                    destinationType: Camera.DestinationType.FILE_URI // DATA_URL for base64 => not recommend due to memory issue
+                });
+        } catch (err) {
+            showError(err);
+        }
+    }
+}
