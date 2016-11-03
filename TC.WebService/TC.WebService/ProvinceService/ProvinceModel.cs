9af1e89feaa9fa01246cdfaf5d08d7343c6e85a8
vvda1984 <vvda1984@gmail.com>
2016-11-03 21:14:01 +0700

    add missing file

---
+++ TC.WebService/TC.WebService/ProvinceService/ProvinceModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace TradeCensus
+{
+    [DataContract]
+    public class ProvinceModel
+    {
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "referenceGeoID")]
+        public int ReferenceGeoID { get; set; }
+    }
+
+    [DataContract]
+    public class DistrictModel
+    {
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "wards")]
+        public List<WardModel> Wards { get; set; }
+    }
+
+    [DataContract]
+    public class WardModel
+    {
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+    }
+}
